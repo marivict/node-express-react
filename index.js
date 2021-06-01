6489fad77ac074371d13deedb93064ca99ad68cc
Maria De La Rosa <m.delarosa@gbh.com.do>
2021-06-01 09:48:03 -0400

    feat: add build component

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-app.use(express.json())
-
 app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
 
 var notes = [
   {
@@ -26,10 +27,6 @@ var notes = [
   }
 ]
 
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
@@ -62,20 +59,14 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body= request.body
-    if(!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
+    
     const note = {
+        id: generateId(),
         content: body.content, 
         important: body.important || false,
         date: new Date(),
-        id: generatedId(),
     }
     notes = notes.concat(note)
-
     response.json(note)
 })
 
@@ -85,7 +76,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
